2dd85a94a71e80ce85ce0c459f6ba3ea9a3e2788
Peter <skwortsow.petia@yandex.ru>
2023-10-17 15:31:04 +0300

    английские буквы в input

--- script.js
+++ script.js
@@ -8,42 +8,77 @@ let inputs = document.querySelectorAll('input');
 let totalPriceElement = document.querySelector('.calc-price-text'); // текст выбора
 let totalPrice = document.querySelector('#total-price'); // итоговая цена
 
-let div = document.createElement("div");
+
+
 
 function calculate () {
     let total1 = 0;
     let total2 = 0;
     let total3 = 0;
 
-    let text = '';
 
     if (film.checked) {
         total1 = parseFloat(film.value);
-        text = film.name;
-        
+        str = film.name;
+        if (!film.checked) { 
+            totalPriceElement.removeChild(str);
+         }
 	}
 
 	if (bigBook.checked) {
         total2 = parseFloat(bigBook.value);
-        text = bigBook.name;
-
+        str = bigBook.name;
+        
+        if (!bigBook.checked) { 
+            totalPriceElement.removeChild(str);
+         }
 	}
 
 	if (miniBook.checked) {
         total3 = parseFloat(miniBook.value);
-        text = miniBook.name;
-
+        str = miniBook.name;
+        
+        if (!miniBook.checked) { 
+            totalPriceElement.removeChild(str);
+         }
 	}
-    totalPriceElement.innerHTML = text
+    
     totalPrice.innerText = total1 + total2 + total3;
 }
    
-	
+   
+
+
 
     for (const input of inputs) {
-        input.addEventListener('input', function () {
-            calculate ()
-        });
+       input.addEventListener('change', nameLine);
     }
 
+    function nameLine () {
+        calculate ()
+    }
 
+    // ___________________________________
+
+
+    const target = document.getElementById('target');
+const checkboxs = document.querySelectorAll('input[type="checkbox"]');
+
+for (var i = 0; i < checkboxs.length; i++) {
+  checkboxs[i].addEventListener('change', function() {
+    send()
+  }, false);
+}
+
+function send() {
+    let value = '';
+    for (var i = 0; i < checkboxs.length; i++) {
+      let el = checkboxs[i];
+      if (el.checked) {
+      value += ', ' + el.name;
+      }
+    }
+    target.value = value.replace(/^\, +/g, '');
+  }
+  
+  send();
